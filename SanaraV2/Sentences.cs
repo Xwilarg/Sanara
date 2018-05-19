e51b3d1c0f36741505599f91e0cdfaeeba59a0f4
Xwilarg <xwilarg@yahoo.fr>
2018-05-19 16:34:54 +0200

    Fix typo in help menu

--- SanaraV2/Sentences.cs
+++ SanaraV2/Sentences.cs
@@ -175,7 +175,7 @@ namespace SanaraV2
             embed.AddField("Settings Module", noCommandAvailable);
             embed.AddField("Visual Novel Module",
                 "**Vn [visual novel]:** Give informations about a visual novel");
-            embed.AddField("Visual Novel Module",
+            embed.AddField("Xkcd Module",
                 "**Xkcd [(optional) comic id]:** Give a random xkcd comic");
             embed.AddField("YouTube Module",
                 "**Youtube [keywords]:** Give a YouTube video given some keywords");
