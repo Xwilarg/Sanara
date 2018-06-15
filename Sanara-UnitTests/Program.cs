5d2b6769654e9bd7b40e43299eb2f1d2f99b1f83
Xwilarg <xwilarg@yahoo.fr>
2018-06-15 02:43:58 +0200

    Add tests on Jisho module

--- Sanara-UnitTests/Program.cs
+++ Sanara-UnitTests/Program.cs
@@ -6,9 +6,21 @@ namespace Sanara_UnitTests
     public class Program
     {
         [Fact]
-        public void CeckEmptyAddArgs()
+        public void TLinguisticModule()
         {
-            Assert.True(Utilities.addArgs(new string[] { }) == null);
+           Assert.True(LinguistModule.toKatakana(LinguistModule.fromHiragana("oranji じゅいす")) == "オランジ ジュイス");
+        }
+
+        [Fact]
+        public void ToHiragana()
+        {
+            Assert.True(LinguistModule.toHiragana(LinguistModule.fromKatakana("oranji ジュイス")) == "おらんじ じゅいす");
+        }
+
+        [Fact]
+        public void ToRomaji()
+        {
+            Assert.True(LinguistModule.fromHiragana(LinguistModule.fromKatakana("おらんじ ジュイス")) == "oranji juisu");
         }
     }
 }
