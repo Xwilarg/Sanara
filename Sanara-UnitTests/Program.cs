6c5c8f5fe34200d8f6f471e5b02137ea93fbf2a5
Xwilarg <xwilarg@yahoo.fr>
2018-11-08 21:47:00 +0100

    Add unit tests

--- Sanara-UnitTests/Program.cs
+++ Sanara-UnitTests/Program.cs
@@ -1,16 +1,35 @@
 ﻿using Xunit;
-using SanaraV2.Features.Entertainment;
 using System;
+using SanaraV2.Features.NSFW;
+using SanaraV2.Features.Entertainment;
+using System.Net;
 
 namespace Sanara_UnitTests
 {
     public class Program
     {
+        private static bool IsLinkValid(string url)
+        {
+            if (url.StartsWith("http://") || url.StartsWith("https://"))
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(url);
+                    request.Method = "HEAD";
+                    request.GetResponse();
+                    return (true);
+                }
+                catch (WebException)
+                { }
+            }
+            return (false);
+        }
+
         [Fact]
         public async void TestAnime()
         {
             var result = await AnimeManga.SearchAnime(true, ("Gochuumon wa Usagi desu ka?").Split(' '));
-            Assert.Equal(Error.AnimeManga.None, result.error);
+            Assert.Equal(SanaraV2.Features.Entertainment.Error.AnimeManga.None, result.error);
             Assert.NotNull(result.answer);
             Assert.Equal("Gochuumon wa Usagi desu ka?", result.answer.name);
             Assert.Equal("https://media.kitsu.io/anime/poster_images/8095/original.jpg?1408463456", result.answer.imageUrl);
@@ -22,6 +41,32 @@ namespace Sanara_UnitTests
             Assert.Equal(new DateTime(2014, 6, 26), result.answer.endDate);
             Assert.Equal("Teens 13 or older", result.answer.ageRating);
             Assert.InRange(result.answer.synopsis.Length, 800, 1200);
+        }
+
+        [Fact]
+        public async void TestDoujinshi()
+        {
+            var result = await Doujinshi.SearchDoujinshi(false, new string[] { "color", "english" }, new Random());
+            Assert.Equal(SanaraV2.Features.NSFW.Error.Doujinshi.None, result.error);
+            Assert.True(IsLinkValid(result.answer.url));
+        }
+
+        [Fact]
+        public async void TestBooruSafe()
+        {
+            var result = await Booru.SearchBooru(false, null, new BooruSharp.Booru.Safebooru());
+            Assert.Equal(SanaraV2.Features.NSFW.Error.Booru.None, result.error);
+            Assert.Equal(Discord.Color.Green, result.answer.colorRating);
+            Assert.True(IsLinkValid(result.answer.url));
+        }
+
+        [Fact]
+        public async void TestBooruNotSafe()
+        {
+            var result = await Booru.SearchBooru(false, new string[] { "pussy" }, new BooruSharp.Booru.Gelbooru());
+            Assert.Equal(SanaraV2.Features.NSFW.Error.Booru.None, result.error);
+            Assert.Equal(Discord.Color.Red, result.answer.colorRating);
+            Assert.True(IsLinkValid(result.answer.url));
         }
     }
 }
