bb016491970948f9fa174ceb22fce2a8cd91859c
Xwilarg <xwilarg@yahoo.fr>
2019-04-25 18:34:02 +0200

    Fix typos in installation script

--- install.sh
+++ install.sh
@@ -7,18 +7,18 @@ if [ ! -d SanaraV2\\bin\\Debug\\Saves ]
 then
 	mkdir -p SanaraV2\\bin\\Debug\\Saves
 fi
-if [ ! -d Sanara.Unit-Tests\\bin\\Debug\\Saves ]
+if [ ! -d Sanara-UnitTests\\bin\\Debug\\Saves ]
 then
-	mkdir -p Sanara.Unit-Tests\\bin\\Debug\\Saves
+	mkdir -p Sanara-UnitTests\\bin\\Debug\\Saves
 fi
 curl -LO https://files.zirk.eu/Sanara/AnimeTags.dat
-cp AnimeTags.dat Sanara.Unit-Tests\\bin\\Debug\\Saves
+cp AnimeTags.dat Sanara-UnitTests\\bin\\Debug\\Saves
 mv AnimeTags.dat SanaraV2\\bin\\Debug\\Saves
 curl -LO https://files.zirk.eu/Sanara/BooruTriviaTags.dat
-cp BooruTriviaTags.dat Sanara.Unit-Tests\\bin\\Debug\\Saves
+cp BooruTriviaTags.dat Sanara-UnitTests\\bin\\Debug\\Saves
 mv BooruTriviaTags.dat SanaraV2\\bin\\Debug\\Saves
 curl -LO https://files.zirk.eu/Sanara/shiritoriWords.dat
-cp shiritoriWords.dat Sanara.Unit-Tests\\bin\\Debug\\Saves
+cp shiritoriWords.dat Sanara-UnitTests\\bin\\Debug\\Saves
 mv shiritoriWords.dat SanaraV2\\bin\\Debug\\Saves
 echo -e "\n\e[92mInstallation succeed, don't forget to compile Sanara using Visual Studio\nDefault output directory is at SanaraV2/bin/Debug."
 echo -e "\nBefore launching Sanara, you must start ReThinkdb.\e[0m"
