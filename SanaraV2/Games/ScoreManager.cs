95af95558d5647fe2d47ba14b958aaa105a108b8
Xwilarg <xwilarg@yahoo.fr>
2019-04-29 12:27:25 +0200

    Fix score sending

--- SanaraV2/Games/ScoreManager.cs
+++ SanaraV2/Games/ScoreManager.cs
@@ -13,6 +13,7 @@
 /// You should have received a copy of the GNU General Public License
 /// along with Sanara.  If not, see<http://www.gnu.org/licenses/>.
 
+using Discord;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,30 +33,37 @@ namespace SanaraV2.Games
             {
                 APreload preload = (APreload)Activator.CreateInstance(game.Item1);
                 string gameName = preload.GetGameName();
-                if (scores.ContainsKey(gameName))
+
+                // Prepare array containing all games serverId/score
+                List<Tuple<string, string>> allScores = new List<Tuple<string, string>>();
+                foreach (var elem in scores)
                 {
-                    List<Tuple<string, int>> best = new List<Tuple<string, int>>();
-                    var allScores = scores[gameName];
-                    string bestServer;
-                    int bestScore;
-                    while (best.Count < 3 && allScores.Count > 0)
-                    {
-                        bestServer = null;
-                        bestScore = -1;
-                        foreach (var elem in allScores)
-                        {
-                            int score = int.Parse(elem.Value);
-                            if (bestServer == null || score > bestScore)
-                            {
-                                bestServer = elem.Key;
-                                bestScore = score;
-                            }
-                        }
-                        allScores.Remove(bestServer);
-                        best.Add(new Tuple<string, int>(Program.p.GetName(bestServer), bestScore));
-                    }
-                    finalStr.Append(string.Join("|", best.Select(x => x.Item1 + "|" + x.Item2)));
+                    if (elem.Value.ContainsKey(gameName))
+                        allScores.Add(new Tuple<string, string>(elem.Key, elem.Value[gameName].Split('|')[0]));
                 }
+                List<Tuple<string, int>> best = new List<Tuple<string, int>>();
+                string bestServer;
+                int bestScore;
+                List<string> alreadySaid = new List<string>();
+                while (best.Count < 3 && allScores.Count > alreadySaid.Count)
+                {
+                    bestServer = null;
+                    bestScore = -1;
+                    foreach (var elem in allScores)
+                    {
+                        int score = int.Parse(elem.Item2);
+                        if (!alreadySaid.Contains(elem.Item1) && (bestServer == null || score > bestScore))
+                        {
+                            bestServer = elem.Item1;
+                            bestScore = score;
+                        }
+                    }
+                    alreadySaid.Add(bestServer);
+                    IGuild guild = Program.p.client.GetGuild(ulong.Parse(bestServer));
+                    if (guild != null)
+                        best.Add(new Tuple<string, int>(Program.p.GetName(Program.p.client.GetGuild(ulong.Parse(bestServer)).Name), bestScore));
+                }
+                finalStr.Append(string.Join("|", best.Select(x => x.Item1 + "|" + x.Item2)) + "$");
             }
             return (finalStr.ToString());
         }
