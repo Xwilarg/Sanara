0f71642a72d3ac18316a6c6b2e099c26428d5486
Xwilarg <xwilarg@yahoo.fr>
2019-06-09 14:50:27 +0200

    Fix install.sh for radio DLLs

--- install.sh
+++ install.sh
@@ -15,15 +15,12 @@ mv BooruTriviaTags.dat SanaraV2\\bin\\Debug\\Saves
 curl -LO https://files.zirk.eu/Sanara/shiritoriWords.dat
 cp shiritoriWords.dat Sanara-UnitTests\\bin\\Debug\\Saves
 mv shiritoriWords.dat SanaraV2\\bin\\Debug\\Saves
-curl -LO https://dsharpplus.emzi0767.com/natives/vnext_natives_win32_x64.zip
-yes | unzip vnext_natives_win32_x64.zip
-rm vnext_natives_win32_x64.zip
-cp libopus.dll Sanara-UnitTests\\bin\\Debug
-mv libopus.dll SanaraV2\\bin\\Debug
+curl -LO https://discord.foxbot.me/binaries/win32/opus.dll
+cp opus.dll Sanara-UnitTests\\bin\\Debug
+mv opus.dll SanaraV2\\bin\\Debug
+curl -LO https://discord.foxbot.me/binaries/win32/libsodium.dll
 cp libsodium.dll Sanara-UnitTests\\bin\\Debug
 mv libsodium.dll SanaraV2\\bin\\Debug
-rm libopus.dll.checksums
-rm libsodium.dll.checksums
 curl -LO https://dsharpplus.emzi0767.com/natives/ffmpeg_win32_x64.zip
 yes | unzip ffmpeg_win32_x64.zip
 rm ffmpeg_win32_x64.zip
