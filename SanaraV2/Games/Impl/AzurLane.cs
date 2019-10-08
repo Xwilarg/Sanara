185cd6ff7e4e087c01d0c7b2431c1c95c3739c0b
Xwilarg <xwilarg@yahoo.fr>
2019-10-08 22:16:52 +0900

    Clean code

--- SanaraV2/Games/Impl/AzurLane.cs
+++ SanaraV2/Games/Impl/AzurLane.cs
@@ -78,7 +78,7 @@ namespace SanaraV2.Games.Impl
             {
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
                 return (new Tuple<string[], string[]>(
-                    new[] { "https://azurlane.koumakan.jp" + Regex.Match(hc.GetStringAsync("https://azurlane.koumakan.jp/" + curr).GetAwaiter().GetResult(),
+                    new[] { "https://azurlane.koumakan.jp" + Regex.Match(await hc.GetStringAsync("https://azurlane.koumakan.jp/" + curr),
                     "src=\"(\\/w\\/images\\/thumb\\/[^\\/]+\\/[^\\/]+\\/[^\\/]+\\/[0-9]+px-" + Uri.EscapeDataString(curr) + ".png)").Groups[1].Value },
                     allNames.ToArray()
                 ));
