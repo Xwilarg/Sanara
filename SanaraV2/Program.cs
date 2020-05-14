2779d0f97d9c452591b5487d54c89c185c161a55
Xwilarg <xwilarg@yahoo.fr>
2020-05-14 17:46:28 +0200

    Fix CI compile error

--- SanaraV2/Program.cs
+++ SanaraV2/Program.cs
@@ -446,35 +446,37 @@ namespace SanaraV2
                 ARKNIGHTS_SKILLS = new Dictionary<string, Tuple<string, string>>();
                 ARKNIGHTS_DESCRIPTIONS = new Dictionary<string, string>();
 
-                using HttpClient hc = new HttpClient();
-                ARKNIGHTS_GENERAL = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/gamedata/zh_CN/gamedata/excel/character_table.json"));
-                dynamic j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/tl-unreadablename.json"));
-                foreach (dynamic elem in j)
+                using (HttpClient hc = new HttpClient())
                 {
-                    ARKNIGHTS_ALIASES.Add(Features.Utilities.CleanWord((string)elem.name_en), ((string)elem.name).ToUpper());
-                }
-                j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/tl-tags.json"));
-                foreach (dynamic elem in j)
-                {
-                    ARKNIGHTS_TAGS.Add((string)elem.tag_cn, Features.Utilities.CleanWord((string)elem.tag_en));
-                }
-                j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/tl-akhr.json"));
-                foreach (dynamic elem in j)
-                {
-                    ARKNIGHTS_DESCRIPTIONS.Add((string)elem.name_en, (string)elem.characteristic_en);
-                }
-                var tab = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/ace/tl-skills.json"));
-                j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/gamedata/zh_CN/gamedata/excel/skill_table.json"));
-                foreach (var elem in tab)
-                {
-                    string description = elem.Value.desc[0];
-                    foreach (var d in j[elem.Key].levels[0].blackboard)
+                    ARKNIGHTS_GENERAL = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/gamedata/zh_CN/gamedata/excel/character_table.json"));
+                    dynamic j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/tl-unreadablename.json"));
+                    foreach (dynamic elem in j)
                     {
-                        description = description.Replace("{" + d.key + "}", (string)d.value);
+                        ARKNIGHTS_ALIASES.Add(Features.Utilities.CleanWord((string)elem.name_en), ((string)elem.name).ToUpper());
                     }
-                    description = Regex.Replace(description, "{([0-9.-]+):\\.0f}", "$1");
-                    description = Regex.Replace(description, "{([0-9.-]+):\\.0%}", "$1%");
-                    ARKNIGHTS_SKILLS.Add(elem.Key, new Tuple<string, string>((string)elem.Value.name, description));
+                    j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/tl-tags.json"));
+                    foreach (dynamic elem in j)
+                    {
+                        ARKNIGHTS_TAGS.Add((string)elem.tag_cn, Features.Utilities.CleanWord((string)elem.tag_en));
+                    }
+                    j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/tl-akhr.json"));
+                    foreach (dynamic elem in j)
+                    {
+                        ARKNIGHTS_DESCRIPTIONS.Add((string)elem.name_en, (string)elem.characteristic_en);
+                    }
+                    var tab = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/ace/tl-skills.json"));
+                    j = JsonConvert.DeserializeObject(await hc.GetStringAsync("https://aceship.github.io/AN-EN-Tags/json/gamedata/zh_CN/gamedata/excel/skill_table.json"));
+                    foreach (var elem in tab)
+                    {
+                        string description = elem.Value.desc[0];
+                        foreach (var d in j[elem.Key].levels[0].blackboard)
+                        {
+                            description = description.Replace("{" + d.key + "}", (string)d.value);
+                        }
+                        description = Regex.Replace(description, "{([0-9.-]+):\\.0f}", "$1");
+                        description = Regex.Replace(description, "{([0-9.-]+):\\.0%}", "$1%");
+                        ARKNIGHTS_SKILLS.Add(elem.Key, new Tuple<string, string>((string)elem.Value.name, description));
+                    }
                 }
             }
             catch (Exception e) // If somehow something happens
