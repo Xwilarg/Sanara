a99ce58c41244682205779ba86f94649376f8ac6
Xwilarg <xwilarg@yahoo.fr>
2020-09-13 21:12:37 +0200

    Add quote command

--- SanaraV3/Module/Tool/CommunicationModule.cs
+++ SanaraV3/Module/Tool/CommunicationModule.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using SanaraV3.Exception;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,12 +14,35 @@ namespace SanaraV3.Module.Tool
             await QuoteInternalAsync(msg);
         }
 
+        [Command("Quote", RunMode = RunMode.Async)]
+        public async Task QuoteAsync(IUser user)
+        {
+            var msgs = await Context.Channel.GetMessagesAsync().FlattenAsync();
+            var msg = msgs.First(x => x.Author.Id == user.Id);
+            if (msg == null)
+                throw new CommandFailed("This user didn't post any message here recently.");
+            await QuoteInternalAsync(msg);
+        }
+
+        [Command("Quote", RunMode = RunMode.Async), Priority(-1)]
+        public async Task QuoteAsync()
+        {
+            var msgs = await Context.Channel.GetMessagesAsync().FlattenAsync();
+            var msg = msgs.First(x => x.Author.Id == Context.User.Id);
+            if (msg == null)
+                throw new CommandFailed("You didn't post any message here recently.");
+            await QuoteInternalAsync(msg);
+        }
+
         private async Task QuoteInternalAsync(IMessage msg)
         {
             await ReplyAsync(embed: new EmbedBuilder
             {
-                ThumbnailUrl = msg.Author.GetAvatarUrl(),
-                Title = msg.Author.ToString(),
+                Author = new EmbedAuthorBuilder
+                {
+                    IconUrl = msg.Author.GetAvatarUrl(),
+                    Name = msg.Author.ToString()
+                },
                 Description = msg.Content.Length == 0 && msg.Embeds.Count == 1 ? msg.Embeds.ToArray()[0].Description : msg.Content,
                 Footer = new EmbedFooterBuilder
                 {
