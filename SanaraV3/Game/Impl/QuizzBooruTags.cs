f0dc0bdd3aa53fa5938e924cfe78ab2774848309
Xwilarg <xwilarg@yahoo.fr>
2020-10-08 17:25:45 +0200

    Fix invalid characters

--- SanaraV3/Game/Impl/QuizzBooruTags.cs
+++ SanaraV3/Game/Impl/QuizzBooruTags.cs
@@ -2,6 +2,7 @@
 using SanaraV3.Game.Preload;
 using System;
 using System.Linq;
+using System.Web;
 
 namespace SanaraV3.Game.Impl
 {
@@ -17,6 +18,8 @@ namespace SanaraV3.Game.Impl
             var results = _booru.GetRandomPostsAsync(3, _current.ImageUrl).GetAwaiter().GetResult();
             if (results.Length < 3)
                 throw new IndexOutOfRangeException("No result with tags " + _current.ImageUrl);
+
+            _current = new Preload.Result.QuizzPreloadResult(_current.ImageUrl, _current.Answers.Select(x => HttpUtility.UrlDecode(x)).ToArray());
 
             return results.Select(x => x.fileUrl.AbsoluteUri).ToArray();
         }
