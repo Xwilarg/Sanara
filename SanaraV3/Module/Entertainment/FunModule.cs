47acee24d411218c7128a4d36e25b61bd3a345f8
Xwilarg <xwilarg@yahoo.fr>
2021-01-14 18:58:27 +0100

    Add VNQuote command

--- SanaraV3/Module/Entertainment/FunModule.cs
+++ SanaraV3/Module/Entertainment/FunModule.cs
@@ -3,10 +3,14 @@ using Discord.Commands;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SanaraV3.Exception;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
+using VndbSharp;
+using VndbSharp.Models;
 using WebSocketSharp;
 
 namespace SanaraV3.Help
@@ -19,6 +23,7 @@ namespace SanaraV3.Help
             _help.Add(("Entertainment", new Help("Fun", "Inspire", new Argument[0], "Get a random 'inspirational' quote using machine learning.", new string[0], Restriction.None, null)));
             _help.Add(("Entertainment", new Help("Fun", "Complete", new[] { new Argument(ArgumentType.MANDATORY, "sentence") }, "Complete the given sentence using machine learning.", new string[0], Restriction.None, "Complete Why can't cats just")));
             _help.Add(("Entertainment", new Help("Fun", "Photo", new[] { new Argument(ArgumentType.OPTIONAL, "query") }, "Get a photo given some search terms, if none is provided, get a random one.", new string[0], Restriction.None, "Photo France")));
+            _help.Add(("Entertainment", new Help("Fun", "VNQuote", new Argument[0], "Get a quote from a random Visual Novel.", new string[0], Restriction.Nsfw, null)));
         }
     }
 }
@@ -30,6 +35,22 @@ namespace SanaraV3.Module.Entertainment
     /// </summary>
     public sealed class FunModule : ModuleBase
     {
+        [Command("VNQuote"), RequireNsfw]
+        public async Task VNQuote()
+        {
+            var html = await StaticObjects.HttpClient.GetStringAsync("https://vndb.org");
+            var match = Regex.Match(html, "footer\">\"<a href=\"\\/v([0-9]+)\"[^>]+>([^<]+)+<");
+            var id = match.Groups[1].Value;
+            var vn = (await StaticObjects.VnClient.GetVisualNovelAsync(VndbFilters.Id.Equals(uint.Parse(id)), VndbFlags.FullVisualNovel)).ToArray()[0];
+            await ReplyAsync(embed: new EmbedBuilder
+            {
+                Title = "From " + vn.Name,
+                Url = "https://vndb.org/v" + id,
+                Description = match.Groups[2].Value,
+                Color = Color.Blue
+            }.Build());
+        }
+
         [Command("Photo", RunMode = RunMode.Async)]
         public async Task PhotoAsync()
         {
