d81909f684e85b56e51434646f79219af3e16664
Xwilarg <xwilarg@yahoo.fr>
2022-01-07 09:46:39 +0100

    Improve deprecation notice

--- Sanara/Module/DeprecationNotice.cs
+++ Sanara/Module/DeprecationNotice.cs
@@ -7,7 +7,8 @@ namespace Sanara.Module
         [Command("Botinfo"), Alias("Help", "Ping", "Gdpr",
             "Anime", "Manga", "LightNovel", "Drama",
             "Safebooru", "Booru", "E629", "E926", "Gelbooru", "Rule34", "Konachan",
-            "Complete", "VNQuote", "Inspire", "Translate")]
+            "Complete", "VNQuote", "Inspire", "Translate",
+            "AdultVideo", "Doujinshi", "Cosplay", "Play")]
         public async Task Deprecated(params string[] _)
         {
             await ReplyAsync(
