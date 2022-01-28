8f70a3ae26b36725105b760541ddb860241a2faf
Xwilarg <xwilarg@yahoo.fr>
2022-01-28 21:57:15 +0100

    Fix FateGO preload

--- Sanara/Game/Preload/Impl/Static/FateGO.cs
+++ Sanara/Game/Preload/Impl/Static/FateGO.cs
@@ -11,14 +11,14 @@ namespace Sanara.Game.Preload.Impl.Static
             {
                 string html = StaticObjects.HttpClient.GetStringAsync("https://fategrandorder.fandom.com/wiki/" + servantClass).GetAwaiter().GetResult();
                 html = html.Split(new[] { "navbox mw-collapsible" }, StringSplitOptions.None)[0]; // Remove useless things at ending
-                html = string.Join("", html.Split(new[] { "<table class=\"wikitable\"" }, StringSplitOptions.None).Skip(1));
+                html = string.Join("", html.Split(new[] { "<table class=\"wikitable\"" }, StringSplitOptions.None).Skip(2));
                 foreach (string s in html.Split(new[] { "<td>" }, StringSplitOptions.None))
                 {
                     Match match = Regex.Match(s, "<a href=\"\\/wiki\\/([^\"]+)\"( |\t)*title=\"[^\"]+\">");
                     if (match.Success && !s.Contains("Unplayable")) // We only take servants that can be played
                     {
                         string name = match.Groups[1].Value;
-                        if (!_characters.Contains(name)) // Some characters are here multiple times
+                        if (!_characters.Contains(name) && name != "Event_Reward") // Some characters are here multiple times
                         {
                             _characters.Add(name);
                         }
