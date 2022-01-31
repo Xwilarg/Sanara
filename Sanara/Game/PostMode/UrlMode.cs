664b9ab6d19a9ee99d83dde042224fb79880cf09
Xwilarg <xwilarg@yahoo.fr>
2022-01-31 08:57:34 +0100

    Add exception handling for URL post mode

--- Sanara/Game/PostMode/UrlMode.cs
+++ Sanara/Game/PostMode/UrlMode.cs
@@ -1,5 +1,5 @@
 ﻿using Discord;
-using Sanara;
+using Sanara.Exception;
 
 namespace Sanara.Game.PostMode
 {
@@ -7,12 +7,26 @@ namespace Sanara.Game.PostMode
     {
         public async Task PostAsync(IMessageChannel chan, string text, AGame _)
         {
-            var result = await StaticObjects.HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, text));
-            var length = int.Parse(result.Content.Headers.GetValues("content-length").ElementAt(0));
-            if (length < 8000000)
-                await chan.SendFileAsync((await StaticObjects.HttpClient.GetAsync(text)).Content.ReadAsStream(), "image" + Path.GetExtension(text));
-            else // Too big to be sent on Discord
-                await chan.SendMessageAsync(text);
+            try
+            {
+                var result = await StaticObjects.HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, text));
+                var length = int.Parse(result.Content.Headers.GetValues("content-length").ElementAt(0));
+                if (length < 8000000)
+                    await chan.SendFileAsync((await StaticObjects.HttpClient.GetAsync(text)).Content.ReadAsStream(), "image" + Path.GetExtension(text));
+                else // Too big to be sent on Discord
+                    await chan.SendMessageAsync(text);
+            }
+            catch (Discord.Net.HttpException dne)
+            {
+                if (dne.DiscordCode == DiscordErrorCode.MissingPermissions)
+                {
+                    throw new GameLost("Missing permissions to send files");
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
