86c9e5cc059277cad799741d8397d67d968133ed
Xwilarg <xwilarg@yahoo.fr>
2022-02-01 13:56:07 +0100

    Fix subscription needing defer

--- Sanara/Module/Command/Impl/Subscription.cs
+++ Sanara/Module/Command/Impl/Subscription.cs
@@ -66,7 +66,7 @@ namespace Sanara.Module.Command.Impl
                     }.Build(),
                     callback: SubscribeAsync,
                     precondition: Precondition.GuildOnly | Precondition.AdminOnly,
-                    needDefer: false
+                    needDefer: true
                 )
             };
         }
