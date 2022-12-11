93f544486adf9554f681b8b65f4d2e1143e6de96
Xwilarg <xwilarg@protonmail.com>
2022-12-11 19:48:04 +0100

    Clean anime subscription display

--- Sanara/Subscription/Impl/AnimeSubscription.cs
+++ Sanara/Subscription/Impl/AnimeSubscription.cs
@@ -11,12 +11,18 @@ namespace Sanara.Subscription.Impl
         public async Task<FeedItem[]> GetFeedAsync(int current, bool _)
         {
             List<FeedItem> items = new();
-            foreach (var info in await GetFeedInternalAsync())
+            var feed = await GetFeedInternalAsync();
+            for (int i = 0; i < feed.Length; i++)
             {
+                var info = feed[i];
+                while (i + 1 < feed.Length && info.media.id == feed[i + 1].media.id && feed[i].episode == feed[i + 1].episode - 1)
+                {
+                    i++;
+                }
                 items.Add(new FeedItem(info.id.GetHashCode(), new EmbedBuilder
                 {
                     Color = Color.Blue,
-                    Title = $"{info.media.title.romaji} (Episode {info.episode})",
+                    Title = $"{info.media.title.romaji} (Episode {info.episode}" + (info.episode == feed[i].episode ? "" : $" - {feed[i].episode}") + ")",
                     Description = info.media.description,
                     Url = $"https://anilist.co/anime/{info.media.id}",
                     ImageUrl = info.media.coverImage.large
