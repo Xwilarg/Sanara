68b0ae75b23ef7671b8ac243affd61691aec5439
Xwilarg <xwilarg@protonmail.com>
2024-09-19 19:26:53 +0100

    Improve source command

--- Sanara/Module/Utility/Tool.cs
+++ Sanara/Module/Utility/Tool.cs
@@ -11,17 +11,21 @@ public static class Tool
     {
         var web = provider.GetRequiredService<HtmlWeb>();
         var html = web.Load("https://saucenao.com/search.php?url=" + Uri.EscapeDataString(paramUrl));
-        var answers = html.DocumentNode.SelectNodes("//div[contains(@class, 'result')]");
-        if (!answers.Any())
+        var answer = html.DocumentNode.SelectSingleNode("//div[contains(@class, 'result')]");
+        if (answer == null)
             throw new CommandFailed("I didn't find the source of this image.");
 
-        var answer = answers[0];
         var url = html.GetElementbyId("resImage0").Attributes["src"].Value;
         var compatibility = answer.SelectSingleNode("//div[contains(@class, 'resultsimilarityinfo')]").InnerHtml;
         var content = answer.SelectSingleNode("//div[contains(@class, 'resultcontentcolumn')]").InnerHtml;
 
+        var link = answer.SelectSingleNode("//div[contains(@class, 'resultcontentcolumn')]").SelectSingleNode("a");
+        var linkUrl = link == null ? null : link.Attributes["href"].Value;
+
         return new EmbedBuilder
         {
+            Title = "Closest match",
+            Url = linkUrl,
             ImageUrl = url,
             Description = Utils.CleanHtml(content),
             Color = Color.Blue,
